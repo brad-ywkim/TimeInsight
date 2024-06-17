9a7675d2fa90d601e56cad9aee6aaf3225b04c17
Jade <dev.jadekim@gmail.com>
2024-06-18 02:36:55 +0900

    update: BCryptPasswordEncoder

--- timeinsight/src/main/java/kr/co/WebConfig.java
+++ timeinsight/src/main/java/kr/co/WebConfig.java
@@ -2,6 +2,7 @@ package kr.co;
 
 import kr.co.util.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,7 +17,9 @@ public class WebConfig implements WebMvcConfigurer {
     private LoginInterceptor LoginInterceptor;
 
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder;
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
